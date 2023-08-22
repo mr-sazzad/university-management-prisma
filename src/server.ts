1fc5bcd08a854bfacaa9803da47d91ed97fbf873
mr-sazzad <main.sazza@gmail.com>
2023-08-23 01:52:51 +0600

    server.ts file commited successfully

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,76 @@
+import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4001;
 
-const connectingString = () => {
+let server: Server | null = null;
+
+process.on('uncaughtException', error => {
+  console.error('Uncaught Exception:', error);
+  process.exit(1);
+});
+
+const connectToDatabase = async () => {
   try {
-    mongoose.connect(process.env.DATABASE_URL as string);
-    console.log('🛢️ database connected successfully !');
-
-    app.listen(port, () => {
-      console.log(
-        `🏬 University management listening on port http://localhost:${port}`,
-      );
-    });
-
-    app.listen();
-  } catch (error: any) {
-    console.log('😭 Failed to connect database', error);
+    await mongoose.connect(process.env.DATABASE_URL as string);
+    console.log('🛢️ Database connected successfully!');
+  } catch (error) {
+    console.error('😭 Failed to connect to the database', error);
+    process.exit(1);
   }
 };
 
-connectingString();
+const startServer = () => {
+  server = app.listen(port, () => {
+    console.log(
+      `🏬 University management listening on port http://localhost:${port}`,
+    );
+  });
+};
+
+const handleServerShutdown = async () => {
+  if (server) {
+    server.close(async () => {
+      console.log('Server closed gracefully.');
+      await mongoose.connection.close();
+      process.exit(0);
+    });
+  } else {
+    await mongoose.connection.close();
+    process.exit(0);
+  }
+};
+
+const init = async () => {
+  await connectToDatabase();
+  startServer();
+};
+
+init();
+
+process.on('SIGINT', () => {
+  console.log('Received termination signal. Closing server gracefully...');
+  handleServerShutdown();
+});
+
+process.on('SIGTERM', () => {
+  console.log(
+    'Received termination signal (SIGTERM). Closing server gracefully...',
+  );
+  handleServerShutdown();
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Promise Rejection:');
+  console.error(reason);
+  console.error(promise);
+
+  if (reason instanceof Error) {
+    console.error('Error:', reason.message);
+    console.error('Stack Trace:', reason.stack);
+  }
+
+  console.log('Unhandled Rejection Detected. Closing our server gracefully...');
+  handleServerShutdown();
+});
